629a1f09cbcb6cc94c84055b25938a7a33bd2415
ManvithaC <manvitha.challagundla@gmail.com>
2019-04-16 20:55:31 -0700

    lat lang details manual - working

--- app/src/main/java/com/example/noq/AdminActivity.java
+++ app/src/main/java/com/example/noq/AdminActivity.java
@@ -1,6 +1,11 @@
 package com.example.noq;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,7 +24,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -32,11 +42,19 @@ public class AdminActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+    private PlaceCreateTask mAuthTask = null;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private EditText mPlaceName;
+    private EditText mPlaceLatitude;
+    private EditText mPlaceLongitude;
+    private EditText mPlaceId;
+
+    private View mAdminProgressView;
+    private View mAdminFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +80,11 @@ public class AdminActivity extends AppCompatActivity {
             }
         });
 
-        Button mSignInButton = (Button) findViewById(R.id.locate_me_button);
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
+        mAdminProgressView = findViewById(R.id.admin_create_form);
+        mAdminFormView = findViewById(R.id.admin_progress);
+
+        Button mLocateMeButton = (Button) findViewById(R.id.locate_me_button);
+        mLocateMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("button click", "locate button clicked");
@@ -72,8 +93,131 @@ public class AdminActivity extends AppCompatActivity {
             }
         });
 
+        Button mPlaceCreateButton = (Button) findViewById(R.id.event_create_button);
+        mPlaceCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("button click", "event create button clicked");
+                postPlaceDetailsToServer();
+            }
+        });
+
+        mPlaceName = (EditText) findViewById(R.id.place_name);
+        mPlaceLatitude = (EditText) findViewById(R.id.place_lat);
+        mPlaceLongitude = (EditText) findViewById(R.id.place_long);
+        mPlaceId = (EditText) findViewById(R.id.place_id);
     }
 
+    private void postPlaceDetailsToServer(){
+        if (mAuthTask != null) {
+            return;
+        }
+        String placeName = mPlaceName.getText().toString();
+        String placeLat = mPlaceLatitude.getText().toString();
+        String placeLong = mPlaceLongitude.getText().toString();
+        String placeId = mPlaceId.getText().toString();
+
+        mAuthTask = new PlaceCreateTask(placeName, placeLat, placeLong, placeId );
+        mAuthTask.execute((Void) null);
+    }
+
+    public class PlaceCreateTask extends AsyncTask<Object, String, String> {
+
+        private final String mPlaceName;
+        private final String mPlaceLatitude;
+        private final String mPlaceLongitude;
+        private final String mPlaceId;
+
+        PlaceCreateTask(String placeName, String placeLat, String placeLong, String placeId) {
+            mPlaceName = placeName;
+            mPlaceLatitude = placeLat;
+            mPlaceLongitude = placeLong;
+            mPlaceId = placeId;
+        }
+
+        @Override
+        protected String doInBackground(Object... objects) {
+            HashMap<String, String> newPlace = new HashMap<String, String>();
+            newPlace.put("email", LoginActivity.EMAIL);
+            newPlace.put("name", mPlaceName);
+            newPlace.put("placeId", mPlaceId);
+            newPlace.put("latitude", mPlaceLatitude);
+            newPlace.put("longitude", mPlaceLongitude);
+
+            String returnData ="";
+
+            String url = "https://noqueue-app.herokuapp.com/places";
+            PostUrl postUrl = new PostUrl();
+            try {
+                returnData = postUrl.postData(newPlace, url);
+                Log.d("post new place success", returnData);
+
+                Snackbar.make(mAdminFormView, "Queue created successfully.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return returnData;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if(result == "Successful"){
+                Log.d("Place creation success", result);
+
+                Toast toast =  Toast.makeText(getApplicationContext(), "Place creation successful.",
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mAdminFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mAdminFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mAdminFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mAdminProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mAdminProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mAdminProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mAdminProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mAdminFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
